60b19fd322276ea4b6d1bd8680aa737d28f8bc55
sc81 <slawekczernikowski@gmail.com>
2020-08-05 18:04:14 +0200

    Add a color control to the typography panel

---
+++ src/style-controls/typography/text-color.js
@@ -0,0 +1,50 @@
+/**
+ * WordPress dependencies
+ */
+import { useState } from '@wordpress/element';
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Internal dependencies
+ */
+import NormalHoverButtons from '../../components/normal-hover-buttons';
+import OpenColorPicker from '../../components/open-color-picker';
+import propertyService from '../property-service';
+import { ALL_DEVICES } from '../../constants';
+import Separator from '../../components/separator';
+import Transition from '../transition';
+
+export default function TextColor( props ) {
+	const { selector, attributes, setAttributes } = props;
+	const [ isHover, setIsHover ] = useState( false );
+
+	const currentSelector = isHover ? `${ selector }:hover` : selector;
+
+	const { propValue: color, onChange } = propertyService( {
+		propName: 'color',
+		selector: currentSelector,
+		devices: ALL_DEVICES,
+		attributes,
+		setAttributes,
+	} );
+	return (
+		<>
+			<NormalHoverButtons
+				isHover={ isHover }
+				onChange={ ( value ) => setIsHover( value ) }
+			/>
+			<OpenColorPicker
+				lable={ __( 'Text color' ) }
+				value={ color }
+				onChange={ onChange }
+			/>
+			{ isHover && (
+				<>
+					<Separator />
+					<Transition { ...props } transitionProps={ [ 'color' ] } />
+				</>
+			) }
+			<Separator />
+		</>
+	);
+}
