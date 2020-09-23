83e1df868b1bfd28c98a45e6d5825ba864d5bfff
sc81 <sc81git@gmail.com>
2020-09-23 23:06:49 +0200

    Delete unused constants
    * PLUGIN_SETTINGS_NAME
    * GOOGLE_FONTS_LINK_ID
    * MOBILE_DEVICES_MAX_WIDTH
    * TABLET_DEVICES_MAX_WIDTH
    * PRIMARY_GOOGLE_FONT_VAR
    * SECONDARY_GOOGLE_FONT_VAR
    * TERTIARY_GOOGLE_FONT_VAR
    * FONTS_VARS_STYLE_ID

--- src/constants.js
+++ src/constants.js
@@ -6,22 +6,9 @@ export const CORE_EDIT_POST_STORE_NAME = 'core/edit-post';
 export const CORE_BLOCK_EDITOR_STORE_NAME = 'core/block-editor';
 export const CORE_BLOCKS_STORE_NAME = 'core/blocks';
 
-export const PLUGIN_SETTINGS_NAME = `${ PLUGIN_NAME }_settings`;
-
-export const GOOGLE_FONTS_LINK_ID = PLUGIN_NAME + '-google-fonts';
-
 export const MOBILE_DEVICES = 'Mobile';
 export const TABLET_DEVICES = 'Tablet';
 export const DESKTOP_DEVICES = 'Desktop';
 export const ALL_DEVICES = 'allDevices';
-
-export const MOBILE_DEVICES_MAX_WIDTH = '768'; //??
-export const TABLET_DEVICES_MAX_WIDTH = '1024'; //??
-
-export const PRIMARY_GOOGLE_FONT_VAR = `--${ PLUGIN_NAME }-primary-google-font`;
-export const SECONDARY_GOOGLE_FONT_VAR = `--${ PLUGIN_NAME }-secondary-google-font`;
-export const TERTIARY_GOOGLE_FONT_VAR = `--${ PLUGIN_NAME }-tertiary-google-font`;
-
-export const FONTS_VARS_STYLE_ID = `${ PLUGIN_NAME }-fonts-vars-style`;
 
 export const PLUGIN_COLORS = [ { name: 'orange', color: '#e43b04' } ];
