5f92ee22cc959221bb38d0f4618253f75698a0ef
sc81 <sc81git@gmail.com>
2020-11-02 19:12:42 +0100

    Improve Initial_Css class

--- includes/initial-css.php
+++ includes/initial-css.php
@@ -6,15 +6,31 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 class Initial_Css {
-	public static function build( $input ) {
+	public static function build( array $arr_css ) : string {
+		$desktop = '';
+		$tablet  = '';
+		$mobile  = '';
+		if ( isset( $arr_css['Desktop'] ) ) {
+			$desktop = self::device_css( $arr_css['Desktop'] );
+		}
+		if ( isset( $arr_css['Tablet'] ) ) {
+			$tablet = '@media(max-width: 1024px){' . self::device_css( $arr_css['Tablet'] ) . '}';
+		}
+		if ( isset( $arr_css['Mobile'] ) ) {
+			$mobile = '@media(max-width: 767px){' . self::device_css( $arr_css['Mobile'] ) . '}';
+		}
+		return $desktop . $tablet . $mobile;
+	}
+
+	public static function device_css( array $arr ) : string {
 		$css = '';
-		foreach ( $input as $selector => $props ) {
+		foreach ( $arr as $selector => $props ) {
 			$css .= $selector . '{' . implode( ';', $props ) . ';}';
 		}
 		return $css;
 	}
 
-	public static function get() {
+	public static function get() : string {
 		$css_array = apply_filters(
 			'scblocks_initial_css',
 			array()
