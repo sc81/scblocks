b2a09fc63f6205bda5734e8b398808ce771f2014
sc81 <slawekczernikowski@gmail.com>
2021-04-27 23:28:42 +0200

    Update dist

--- dist/constants.asset.php
+++ dist/constants.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-polyfill'), 'version' => '53b0473badbe28b782c0b8e29cd4b381');
+<?php return array('dependencies' => array('wp-polyfill'), 'version' => '94ef0002413c71fdf125185d9b67e537');
