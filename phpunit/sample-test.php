e429e0b84e762b7a7e1e1924e0020fc60b3c29c0
sc81 <sc81git@gmail.com>
2021-10-31 16:42:00 +0100

    Set up php unit tests

---
+++ phpunit/sample-test.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Scblocks
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+	/**
+	 * A single example test.
+	 */
+	public function test_sample() {
+		$this->assertTrue(true);
+	}
+}
