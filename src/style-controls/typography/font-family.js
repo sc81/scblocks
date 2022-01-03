8608be1a1e5fb62e0115d52ea4fa21a6dc4beb96
sc81 <sc81git@gmail.com>
2022-01-03 21:24:32 +0100

    dropdown

--- src/style-controls/typography/font-family.js
+++ src/style-controls/typography/font-family.js
@@ -2,20 +2,59 @@
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { TextControl } from '@wordpress/components';
+import { TextControl, DropdownMenu } from '@wordpress/components';
+import { useMemo } from '@wordpress/element';
+import { useSelect } from '@wordpress/data';
 
 /**
  * ScBlocks dependencies
  */
 import { ControlWrapper } from '@scblocks/components';
+import { STORE_NAME } from '@scblocks/constants';
+
+function buildControls( fonts, onChange ) {
+	const options = [
+		{
+			title: 'System Font',
+			onClick: () =>
+				onChange(
+					"-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen-Sans, Ubuntu, Cantarell, 'Helvetica Neue', sans-serif"
+				),
+		},
+	];
+	Object.keys( fonts ).forEach( ( fontId ) => {
+		if ( fonts[ fontId ].name ) {
+			options.push( {
+				title: `Google Font - ${ fonts[ fontId ].name } ( ${ fontId } font )`,
+				onClick: () =>
+					onChange( `var(--scblocks-${ fontId }-google-font)` ),
+			} );
+		}
+	} );
+	return options;
+}
 
 export default function FontFamily( { value, onChange, onClear } ) {
+	const siteGoogleFonts = useSelect( ( store ) => {
+		return store( STORE_NAME ).getSiteGoogleFonts();
+	}, [] );
+	const controls = useMemo(
+		() => buildControls( siteGoogleFonts, onChange ),
+		[ siteGoogleFonts ]
+	);
 	return (
 		<ControlWrapper
 			label={ __( 'Font Family', 'scblocks' ) }
 			withoutSelectDevices
 			displayClearButton={ !! value }
 			onClear={ onClear }
+			extraControls={
+				<DropdownMenu
+					icon="ellipsis"
+					label={ __( 'Choose a font', 'scblocks' ) }
+					controls={ controls }
+				/>
+			}
 		>
 			<TextControl
 				value={ value }
