0a4c311be7842749606ab1ed4e3618501245ea98
sc81 <sc81git@gmail.com>
2022-01-22 20:09:44 +0100

    remove consts

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,6 @@
 export const STORE_NAME = 'scblocks';
 export const PLUGIN_NAME = 'scblocks';
 
-export const CORE_STORE_NAME = 'core';
 export const CORE_EDITOR_STORE_NAME = 'core/editor';
 export const CORE_EDIT_POST_STORE_NAME = 'core/edit-post';
 export const CORE_BLOCK_EDITOR_STORE_NAME = 'core/block-editor';
@@ -14,5 +13,3 @@ export const ALL_DEVICES = 'allDevices';
 
 export const OPTIONS_REST_API_PATH = '/scblocks/v1/options';
 export const POST_SETTINGS_REST_API_PATH = '/scblocks/v1/post-settings';
-
-export const ICON_POST_TYPE_NAME = 'scblocks_icon';
