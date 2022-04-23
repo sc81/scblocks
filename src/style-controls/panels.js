018b61dc3be7c0b9147c3a64adea3b7dcb238e1f
sc81 <sc81git@gmail.com>
2022-04-23 23:07:40 +0200

    Reorganize style controls (#131)
    * Reorganize style controls
    
    * add scblocks.stylePanels.afterBackground filter
    
    * remove GridPanel
    
    * remove TextProperty

--- src/style-controls/panels.js
+++ src/style-controls/panels.js
@@ -1,15 +1,14 @@
 /**
  * WordPress dependencies
  */
-import { G, Path, SVG, Button } from '@wordpress/components';
 import { useState, useMemo } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
+import { applyFilters } from '@wordpress/hooks';
 
 /**
  * ScBlocks dependencies
  */
 import { getLastActivePanel, setLastActivePanel } from '@scblocks/css-utils';
-import { PLUGIN_NAME } from '@scblocks/constants';
 
 /**
  * Internal dependencies
@@ -20,81 +19,7 @@ import Space from './space/index';
 import ColorSet from './color-set';
 import BorderPanel from './border-panel';
 import PositionPanel from './position-panel';
-import GridPanel from './grid-panel';
-
-function Panel( {
-	name,
-	label,
-	children,
-	openedPanel,
-	onClickPanel,
-	panelCount,
-} ) {
-	if ( panelCount === 1 ) {
-		return (
-			<div className={ `${ PLUGIN_NAME }-components-panel-body-inner` }>
-				{ children }
-			</div>
-		);
-	}
-	return (
-		<div className="components-panel__body">
-			<h2 className="components-panel__body-title">
-				<Button
-					className="components-panel__body-toggle"
-					onClick={ () => onClickPanel( name ) }
-					aria-expanded={ openedPanel === name }
-				>
-					{ /*
-						Firefox + NVDA don't announce aria-expanded because the browser
-						repaints the whole element, so this wrapping span hides that.
-					*/ }
-					<span aria-hidden="true">
-						{ openedPanel === name ? (
-							<SVG
-								className="components-panel__arrow"
-								width="24px"
-								height="24px"
-								viewBox="0 0 24 24"
-								xmlns="http://www.w3.org/2000/svg"
-							>
-								<G>
-									<Path fill="none" d="M0,0h24v24H0V0z" />
-								</G>
-								<G>
-									<Path d="M12,8l-6,6l1.41,1.41L12,10.83l4.59,4.58L18,14L12,8z" />
-								</G>
-							</SVG>
-						) : (
-							<SVG
-								className="components-panel__arrow"
-								width="24px"
-								height="24px"
-								viewBox="0 0 24 24"
-								xmlns="http://www.w3.org/2000/svg"
-							>
-								<G>
-									<Path fill="none" d="M0,0h24v24H0V0z" />
-								</G>
-								<G>
-									<Path d="M7.41,8.59L12,13.17l4.59-4.58L18,10l-6,6l-6-6L7.41,8.59z" />
-								</G>
-							</SVG>
-						) }
-					</span>
-					{ label }
-				</Button>
-			</h2>
-			{ openedPanel === name && (
-				<div
-					className={ `${ PLUGIN_NAME }-components-panel-body-inner` }
-				>
-					{ children }
-				</div>
-			) }
-		</div>
-	);
-}
+import Panel from './panel';
 
 export default function Panels( props ) {
 	const {
@@ -181,19 +106,17 @@ export default function Panels( props ) {
 					<Background { ...props } />
 				</Panel>
 			) }
-			{ isVisiblePanel.grid && (
-				<Panel
-					name="grid"
-					label={ __( 'Grid', 'scblocks' ) }
-					onClickPanel={ onClickPanel }
-					openedPanel={ openedPanel }
-					panelCount={ panelCount }
-				>
-					<GridPanel
-						{ ...props }
-						selectorSettings={ selectorsSettings[ index ] }
-					/>
-				</Panel>
+			{ applyFilters(
+				'scblocks.stylePanels.afterBackground',
+				null,
+				props,
+				{
+					onClickPanel,
+					openedPanel,
+					isVisiblePanel,
+					panelCount,
+					selectorIndex: index,
+				}
 			) }
 			{ isVisiblePanel.space && (
 				<Panel
