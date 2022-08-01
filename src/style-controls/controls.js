4ae6b3041a004ee32c4e8e309b10815d8ae1385e
sc81 <sc81git@gmail.com>
2022-08-01 23:23:04 +0200

    StyleControls: use PanelBody component
    * new scblocks.elementPanel.className filter

--- src/style-controls/controls.js
+++ src/style-controls/controls.js
@@ -2,13 +2,13 @@
  * WordPress dependencies
  */
 import { useState } from '@wordpress/element';
-import { Icon, chevronDown, chevronUp } from '@wordpress/icons';
+import { PanelBody } from '@wordpress/components';
+import { applyFilters } from '@wordpress/hooks';
 
 /**
  * ScBlocks dependencies
  */
 import { getLastActivePanel, setLastActivePanel } from '@scblocks/css-utils';
-import { PLUGIN_NAME } from '@scblocks/constants';
 
 /**
  * Internal dependencies
@@ -34,36 +34,19 @@ export default function StyleControls( props ) {
 	selectorsSettings.forEach( ( element ) => {
 		if ( element.isActive !== false ) {
 			elements.push(
-				<div
+				<PanelBody
 					key={ element.id }
-					className={ `components-panel__body ${ PLUGIN_NAME }-components-panel-body` }
-					data-expanded={ openedPanel === element.id }
-				>
-					<h2 className="components-panel__body-title">
-						<button
-							className={ `${ PLUGIN_NAME }-components-panel-body-button` }
-							onClick={ () => onClickPanel( element.id ) }
-							aria-expanded={ openedPanel === element.id }
-							type="button"
-						>
-							{ /*
-									 Firefox + NVDA don't announce aria-expanded because the browser
-									 repaints the whole element, so this wrapping span hides that.
-							 */ }
-							<span aria-hidden="true">
-								{ openedPanel === element.id ? (
-									<Icon icon={ chevronUp } />
-								) : (
-									<Icon icon={ chevronDown } />
-								) }
-							</span>
-							{ element.label }
-						</button>
-					</h2>
-					{ openedPanel === element.id && (
-						<Panels { ...props } selectorId={ element.id } />
+					className={ applyFilters(
+						'scblocks.elementPanel.className',
+						'scblocks-element-panel',
+						{ ...props, selectorId: element.id }
 					) }
-				</div>
+					title={ element.label }
+					onToggle={ () => onClickPanel( element.id ) }
+					opened={ openedPanel === element.id }
+				>
+					<Panels { ...props } selectorId={ element.id } />
+				</PanelBody>
 			);
 		}
 	} );
