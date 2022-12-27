1949c0303c7c0ab435f1b5d408db8dbb6e8b05af
slawek <sc81git@gmail.com>
2022-12-27 17:40:45 +0100

    Inspector: improve performance

--- src/block/inspector.js/index.js
+++ src/block/inspector.js/index.js
@@ -1,37 +1,88 @@
 /**
  * WordPress dependencies
  */
-import { PanelBody } from '@wordpress/components';
+import { PanelBody, TabPanel } from '@wordpress/components';
 import { InspectorControls } from '@wordpress/block-editor';
 import { applyFilters } from '@wordpress/hooks';
+import { __ } from '@wordpress/i18n';
+
+/**
+ * ScBlocks dependencies
+ */
+import { PLUGIN_NAME } from '@scblocks/constants';
+import { getLastActivePanel, setLastActivePanel } from '@scblocks/css-utils';
+import { StyleControls } from '@scblocks/style-controls';
 
 /**
  * Internal dependencies
  */
-import ControlsManager from '../controls-manager';
 import IdClassesControls from '../id-classes-controls';
 
+const tabs = [
+	{
+		name: 'main',
+		title: __( 'Main', 'scblocks' ),
+	},
+	{
+		name: 'style',
+		title: __( 'Style', 'scblocks' ),
+	},
+	{
+		name: 'attributes',
+		title: __( 'Attributes', 'scblocks' ),
+	},
+];
+
+function Main( props ) {
+	return applyFilters( `${ props.blockName }.mainControls`, null, props );
+}
+
+function Attrs( props ) {
+	return applyFilters(
+		`${ props.blockName }.htmlAttrControls`,
+		<PanelBody opened>
+			<IdClassesControls { ...props } />
+		</PanelBody>,
+		props
+	);
+}
+
 export default function Inspector( props ) {
-	const { isSelected, name } = props;
+	const { isSelected, name, blockMemo } = props;
 	if ( ! isSelected ) return null;
 	const blockName = name.replace( '/', '.' );
+
+	function onSelect( panelName ) {
+		setLastActivePanel( blockMemo, 'tabPanel', panelName );
+	}
+
 	return (
 		<InspectorControls>
-			<ControlsManager
-				{ ...props }
-				mainControls={ applyFilters(
-					`${ blockName }.mainControls`,
-					null,
-					props
-				) }
-				htmlAttrsControls={ applyFilters(
-					`${ blockName }.htmlAttrControls`,
-					<PanelBody opened>
-						<IdClassesControls { ...props } />
-					</PanelBody>,
-					props
-				) }
-			/>
+			<TabPanel
+				className={ `${ PLUGIN_NAME }-components-tabs` }
+				initialTabName={ getLastActivePanel( blockMemo ).tabPanel }
+				onSelect={ onSelect }
+				tabs={ tabs }
+			>
+				{ ( tab ) => {
+					return (
+						<div
+							className={ `${ PLUGIN_NAME }-components-tab-panel__tab-wrapper` }
+							data-style={ tab.name === 'style' }
+						>
+							{ tab.name === 'main' && (
+								<Main { ...props } blockName={ blockName } />
+							) }
+							{ tab.name === 'style' && (
+								<StyleControls { ...props } />
+							) }
+							{ tab.name === 'attributes' && (
+								<Attrs { ...props } blockName={ blockName } />
+							) }
+						</div>
+					);
+				} }
+			</TabPanel>
 		</InspectorControls>
 	);
 }
