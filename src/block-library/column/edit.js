11e46d642bf0a3fed5bd494b46ccb22df590de0c
slawek <sc81git@gmail.com>
2022-12-29 19:13:09 +0100

    pass more props

--- src/block-library/column/edit.js
+++ src/block-library/column/edit.js
@@ -49,7 +49,8 @@ export default function Edit( props ) {
 					[ htmlClass ]: !! htmlClass,
 				} ),
 			},
-			attributes
+			props,
+			{ uidClass, itemClass }
 		)
 	);
 	const innerBlocksProps = useInnerBlocksProps( blockProps, {
