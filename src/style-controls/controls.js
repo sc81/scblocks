55036a47b9f0a28da560fbae3a4e5c918074f246
slawek <sc81git@gmail.com>
2023-01-06 19:13:39 +0100

    fix React error

--- src/style-controls/controls.js
+++ src/style-controls/controls.js
@@ -15,6 +15,14 @@ import { getLastActivePanel, setLastActivePanel } from '@scblocks/css-utils';
  */
 import Panels from './panels';
 
+function getClass( props, id ) {
+	return applyFilters(
+		'scblocks.elementPanel.className',
+		'scblocks-element-panel',
+		{ ...props, selectorId: id }
+	);
+}
+
 export default function StyleControls( props ) {
 	const { selectorsSettings, blockMemo } = props;
 	const [ openedPanel, setOpenedPanel ] = useState(
@@ -32,15 +40,12 @@ export default function StyleControls( props ) {
 	const elements = [];
 
 	selectorsSettings.forEach( ( element ) => {
+		const className = getClass( props, element.id );
 		if ( element.isActive !== false ) {
 			elements.push(
 				<PanelBody
 					key={ element.id }
-					className={ applyFilters(
-						'scblocks.elementPanel.className',
-						'scblocks-element-panel',
-						{ ...props, selectorId: element.id }
-					) }
+					className={ className }
 					title={ element.label }
 					onToggle={ () => onClickPanel( element.id ) }
 					opened={ openedPanel === element.id }
